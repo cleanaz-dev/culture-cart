22c1638bf9b31427692d7846d321dbcdafbb7245
cleanaz-dev <founder@cleanazcoding.com>
2025-05-06 15:28:03 -0400

    first commit

--- app/category/[slug]/loading.js
+++ app/category/[slug]/loading.js
@@ -2,8 +2,10 @@ import { ShoppingBasket } from "lucide-react";
 
 export default function Loading() {
   return (
-    <div className="flex items-center justify-center h-[60vh] bg-black">
-      <ShoppingBasket className="h-12 w-12 text-amber-300 animate-wiggle" />
+    <div className="bg-black min-h-screen">
+    <div className="flex items-center justify-center h-[60vh] ">
+      <ShoppingBasket className="h-12 w-12 text-amber-300 animate-pulse" />
+    </div>
     </div>
   );
 }
